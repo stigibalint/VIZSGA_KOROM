5b584bdd3b5e5aabc5606a1d207e202ee4dcb155
stigibalint <72031994+stigibalint@users.noreply.github.com>
2025-04-04 08:45:52 +0200

    Add files via upload

---
+++ AutokKonzol/AutokKonzol/Auto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutokKonzol
+{
+    internal class Auto
+    {
+        int sorszam, gyartasiEv, eladottDb, atlagosAr;
+        string marka, modell, szin;
+
+        public Auto(int sorszam, int gyartasiEv, int eladottDb, int atlagosAr, string marka, string modell, string szin)
+        {
+            this.Sorszam = sorszam;
+            this.GyartasiEv = gyartasiEv;
+            this.EladottDb = eladottDb;
+            this.AtlagosAr = atlagosAr;
+            this.Marka = marka;
+            this.Modell = modell;
+            this.Szin = szin;
+        }
+        public Auto(string CSVsor)
+        {
+            var mezo = CSVsor.Split(';');
+            sorszam = int.Parse(mezo[0]);
+            marka = mezo[1];
+            modell = mezo[2];
+            gyartasiEv = int.Parse(mezo[3]);
+            szin = mezo[4];
+            eladottDb=int.Parse(mezo[5]);
+            atlagosAr = int.Parse(mezo[6]);
+        }
+
+        public int Sorszam { get => sorszam; set => sorszam = value; }
+        public int GyartasiEv { get => gyartasiEv; set => gyartasiEv = value; }
+        public int EladottDb { get => eladottDb; set => eladottDb = value; }
+        public int AtlagosAr { get => atlagosAr; set => atlagosAr = value; }
+        public string Marka { get => marka; set => marka = value; }
+        public string Modell { get => modell; set => modell = value; }
+        public string Szin { get => szin; set => szin = value; }
+    }
+}
---
+++ AutokKonzol/AutokKonzol/Program.cs
@@ -0,0 +1,12 @@
+﻿using AutokKonzol;
+
+
+List<Auto> adatok = new List<Auto>();
+File.ReadAllLines("autok.csv").Skip(1).ToList().ForEach(x => adatok.Add(new Auto(x)));
+
+Console.WriteLine($"5. feladat: {adatok.Count} autó található a listában");
+Console.WriteLine($"6. feladat: {Math.Round(adatok.Average(x=> x.AtlagosAr),1)}");
+Console.WriteLine("7. feladat: Az elmúlt 5 évben gyártott autók:");
+adatok.Where(x => x.GyartasiEv >= DateTime.Now.Year - 5).ToList().ForEach(x => Console.WriteLine($"- {x.Marka} {x.Modell}: {x.GyartasiEv}"));
+Console.WriteLine("8. feladat: legsikeresebb márkák listája az eladott darabszám alapján:");
+adatok.OrderByDescending(x=> x.EladottDb).ThenBy(x => x.Marka).ToList().ForEach(x => Console.WriteLine($"- {x.Marka}: {x.EladottDb} darab"));
